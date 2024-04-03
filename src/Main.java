1404d07a2133d87dd7bfe19e663828bf35cb7b87
pedrokkj13 <pedrogabrielofc@hotmail.com>
2024-04-03 16:22:21 -0300

    Condicao Ternaria

--- src/Main.java
+++ src/Main.java
@@ -4,39 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc =  new Scanner(System.in);
-        Locale.setDefault(Locale.US);
 
-        int codigo;
-        int qntdd;
-
-        codigo = sc.nextInt();
-        qntdd  = sc.nextInt();
-
-        double total;
-
-        if (codigo == 1) {
-            total = (double) qntdd * 4;
-            System.out.printf("Total: %.2f", total);
-        }
-
-        else if (codigo == 2) {
-            total = (double) qntdd * 4.50;
-            System.out.printf("Total: %.2f", total);
-        }
-        else if (codigo == 3) {
-            total = (double) qntdd * 5;
-            System.out.printf("Total: %.2f", total);
-        }
-        else if (codigo == 4) {
-            total = (double) qntdd * 2;
-            System.out.printf("Total: %.2f", total);
-        }
-        else if (codigo == 5) {
-            total = (double) qntdd * 1.50;
-            System.out.printf("Total: %.2f", total);
-        }
-
-        sc.close();
     }
 }
---
+++ src/br/com/aprendizado/extraUm/condicaoTernaria.java
@@ -0,0 +1,41 @@
+package br.com.aprendizado.extraUm;
+
+public class condicaoTernaria {
+    public static void main(String args[]) {
+
+        double preco = 34.5;
+
+        /*
+        *
+        * Dica para ler essa condição ternária:
+        * exemplo;
+        * desconto ()
+        * o Parenteses() será reconhecido como if
+        * desconto (preco < 20.0)
+        * if (preco <20)
+        *
+        * desconto (preco < 20.0) ?
+        * O ponto de interrogação será reconhecido como as chaves{}, ou algo como: Minha primeira ação.
+        * desconto (preco < 20.0) ? preco * 0.1
+        * if (preco < 20.0) {
+        *   desconto = preco * 0.1
+        * }
+        *
+        * desconto (preco < 20.0) ? preco * 0.1 :
+        * Os dois pontos ( : ) será reconhecido como else, ou algo como: Se não for a primeira coisa, tente isso.
+        * desconto (preco < 20.0) ? preco * 0.1 : preco * 0.05
+        * if (preco < 20.0) {
+        *   desconto = preco * 0.1
+        * }
+        *
+        * else {
+        *   desconto = preco * 0.05
+        * }
+        *
+        *
+        * Deu pra entender mais ou menos? kk
+        *
+        * */
+
+    }
+}
